8a6c4191c948bbe9af55a90f0c47fce258d1ea0e
AJR <aj@ss.com>
2020-05-04 12:01:02 +0530

    commit from us guy at 12PM

---
+++ src/App.java
@@ -0,0 +1,12 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Code from US Guy:Line1");
+		System.out.println("Code from US Guy:Line2");
+		System.out.println("Code from Us Guy:Line3");
+
+	}
+
+}
+
